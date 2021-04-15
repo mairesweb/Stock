ac563321e0b6c2e89767934a4d692c70972071bb
Dudu Fernandes <duduwlf@gmail.com>
2021-04-14 21:03:39 -0300

    Implementação do Sequelize e mysql2
    Implementação dos frameworks Sequelize e MySQL2 através do npm para o uso do mysql e nodejs.
    
    Ignorar arquivo comandosBD.sql

---
+++ Plataforma Web/comandosBD.sql
@@ -0,0 +1,30 @@
+INSERT INTO clientes(nomeCliente, emailCliente, telefoneCliente) VALUE(
+    "Dudu Fernandes",
+    "duduwlf@gmail.com",
+    "77988267655"
+);
+
+
+create table clientes(
+    nomeCliente varchar(80),
+    emailCliente varchar(50),
+    telefoneCliente varchar(14)
+);
+
+//Listagem de dados
+// * é para selecionar todos os dados
+
+SELECT * FROM clientes;
+
+SELECT * FROM clientes WHERE emailCliente = "duduwlf@gmail.com";
+SELECT * FROM clientes WHERE idadeCliente >= 18;
+
+
+//NUCA EXECUTE O COMANDO DELETE E SET SEM O WHERE!!!!!!!
+//deletar um registro nas tabelas
+DELETE FROM clientes WHERE nomeCliente = "jairo"; 
+
+//atualizar dados na tabela
+UPDATE clientes SET emailCliente = "teste@teste.com" WHERE emailCliente = "irinei@ciclano.gov.br";
+
+UPDATE clientes SET nomeCliente = "Dudu Fernandes" WHERE nomeCliente = "Ciclano dos Fulanos dos Abreu", SET telefoneCliente = "77988267655" WHERE telefoneCliente = "40028922";
